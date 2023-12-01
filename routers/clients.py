7e73b624f4cd68b03251ebf0a064fcdb2af47f80
Kirill <kirill.pomaskin@gmail.com>
2023-12-01 20:50:07 +0200

    Add delete client router

--- routers/clients.py
+++ routers/clients.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from crud.clients import get_clients, add_client
+from crud.clients import get_clients, add_client, delete_client
 from database.database import get_db
 from schemes import schemes
 
@@ -9,12 +9,15 @@ router = APIRouter()
 
 
 @router.get("", response_model=list[schemes.Client])
-def read_clients(db: Session = Depends(get_db)):
-    clients = get_clients(db)
-    return clients
+def get_all_clients(db: Session = Depends(get_db)):
+    return get_clients(db)
 
 
 @router.post("/create", response_model=schemes.Client)
-def create_client(name: str, db: Session = Depends(get_db)):
-    client = add_client(db, name)
-    return client
+def add_the_client(name: str, db: Session = Depends(get_db)):
+    return add_client(db, name)
+
+
+@router.delete("", response_model=schemes.Client)
+def delete_the_client(client_id: int, db: Session = Depends(get_db)):
+    return delete_client(db=db, client_id=client_id)
