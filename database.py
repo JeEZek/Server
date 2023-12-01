7f202b96494521becd3d28153d33f89e1345ac8c
Kirill <kirill.pomaskin@gmail.com>
2023-12-01 19:42:10 +0200

    Add database

---
+++ database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# write your datas
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:testtask@localhost:5432/platform"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
